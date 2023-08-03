7ec8798a31f0b3d98392f6afadac8d16531b99e7
Victorio Nikolaev <vikinik01@abv.bg>
2023-08-03 11:50:29 +0300

    Adding a 'User' repository.

---
+++ Chatify.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Chatify.Domain.Common;
+using Chatify.Domain.Entities;
+
+namespace Chatify.Domain.Repositories;
+
+public interface IUserRepository : IDomainRepository<User, Guid>
+{
+    Task<User?> GetByUsername(
+        string usernameQuery,
+        CancellationToken cancellationToken = default);
+    
+    Task<List<User>?> AllByUserIds(
+        IEnumerable<Guid> userIds,
+        CancellationToken cancellationToken = default);
+}
--- Chatify.Infrastructure/Data/Repositories/UserRepository.cs
+++ Chatify.Infrastructure/Data/Repositories/UserRepository.cs
@@ -1,15 +1,43 @@
-﻿using Chatify.Domain.Entities;
+﻿using Cassandra.Mapping;
+using Chatify.Domain.Repositories;
 using Chatify.Infrastructure.Data.Models;
+using Chatify.Shared.Abstractions.Serialization;
+using StackExchange.Redis;
 using Guid = System.Guid;
 using IMapper = AutoMapper.IMapper;
 using Mapper = Cassandra.Mapping.Mapper;
 
 namespace Chatify.Infrastructure.Data.Repositories;
 
-public sealed class UserRepository : BaseCassandraRepository<Domain.Entities.User, ChatifyUser, Guid>
+public sealed class UserRepository
+    : BaseCassandraRepository<Domain.Entities.User, ChatifyUser, Guid>, IUserRepository
 {
-    public UserRepository(IMapper mapper, Mapper dbMapper)
+    private readonly IDatabase _cache;
+    private readonly ISerializer _serializer;
+
+    public UserRepository(IMapper mapper, Mapper dbMapper, IDatabase cache, ISerializer serializer)
         : base(mapper, dbMapper)
     {
+        _cache = cache;
+        _serializer = serializer;
+    }
+
+    public Task<Domain.Entities.User?> GetByUsername(string usernameQuery,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<List<Domain.Entities.User>?> AllByUserIds(
+        IEnumerable<Guid> userIds, CancellationToken cancellationToken = default)
+    {
+        var users = await _cache.StringGetAsync(
+            userIds.Select(id => new RedisKey($"user:{id.ToString()}")).ToArray()
+        );
+
+        return Mapper.Map<List<Domain.Entities.User>>(
+            users
+                .Select(u =>
+                    _serializer.Deserialize<ChatifyUser>(u.ToString())));
     }
 }
