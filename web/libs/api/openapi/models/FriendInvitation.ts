dc6470f68013d7dae69d69d68a9e68239fc8b944
Victorio <vikinik01@abv.bg>
2023-08-23 12:43:34 +0300

    Adding OpenAPI models.

---
+++ web/libs/api/openapi/models/FriendInvitation.ts
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { FriendInvitationStatus } from './FriendInvitationStatus';
+
+export type FriendInvitation = {
+    id?: string;
+    inviterId?: string;
+    inviteeId?: string;
+    status?: FriendInvitationStatus;
+    createdAt?: string;
+    updatedAt?: string | null;
+};
